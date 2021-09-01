3265ddd30e5b1d4070a6ec4da5195171756460e1
MonsieurVolt <duceptflorian53@gmail.com>
2021-09-02 00:05:23 +0200

    test changes

--- Header1.inl
+++
@@ -1,8 +0,0 @@
-#include "Header1.h"
-#include <iostream>
-template <typename T>
-Test<T>::Test(T a) :_a{ a } {};
-template <typename T>
-T Test<T>::teste() {
-	return _a;
-};
--- Main.cpp
+++ Main.cpp
@@ -1,27 +1,46 @@
 #include <SFML/Graphics.hpp>
-#include "Header1.h"
+#include <cassert>
 #include <iostream>
+#include <map>
+
+
+#include "RessourcesHolder.hpp"
+using namespace std;
+
+namespace Textures
+{
+	enum struct ID
+	{
+		LandScape,
+		Airplane,
+		Missile
+	};
+
+};
 int main()
 {
-	Test<int> a(2);
-	std::cout << a.teste() << "\n";
-	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-	sf::CircleShape shape(100.f);
-	shape.setFillColor(sf::Color::Green);
-
+	sf::RenderWindow window(sf::VideoMode(500, 500), "Ressources management");
+	ResourceHolder<sf::Texture, Textures::ID> textures;
+	textures.load(Textures::ID::Airplane, "Eagle.png");
+	textures.load(Textures::ID::LandScape, "Desert.png");
+	sf::Sprite plane(textures.get(Textures::ID::Airplane));
+	sf::Sprite desert(textures.get(Textures::ID::LandScape));
+	desert.setPosition(sf::Vector2f(300, 300));
 	while (window.isOpen())
+
 	{
 		sf::Event event;
 		while (window.pollEvent(event))
 		{
 			if (event.type == sf::Event::Closed)
+			{
 				window.close();
+			}
 		}
+		window.clear(sf::Color::Red);
+		window.draw(plane);
 
-		window.clear();
-		window.draw(shape);
+		window.draw(desert);
 		window.display();
 	}
-
-	return 0;
 }
